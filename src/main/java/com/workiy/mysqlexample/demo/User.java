d91981403cb874cbdd1ee483abc8bd36e70d99b6
jay-narasimhan <129411091+jay-narasimhan@users.noreply.github.com>
2024-02-01 22:22:48 -0500

    spring-boot web api demo 1. Connecting to MySQL table 2. Two api URL's /all & /user/{id}

---
+++ src/main/java/com/workiy/mysqlexample/demo/User.java
@@ -0,0 +1,34 @@
+package com.workiy.mysqlexample.demo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class User {
+  @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+  private Integer id;
+
+  @Column(name = "username", length = 200)
+  private String username;
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+}
